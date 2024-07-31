11405fcceb80ff9c05a6a5a5b6b4a12e6b3aa43d
Soham Shiraskar <soham.shiraskar@ofbusiness.in>
2024-07-31 16:23:33 +0530

    Initial Setup

---
+++ src/test/java/com/example/jsonData/JsonDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jsonData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
